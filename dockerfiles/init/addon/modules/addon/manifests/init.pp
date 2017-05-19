c3a7d4aa85b33b0db4ba761374f0497f89785ba2
Roman Iuvshin <riuvshin@codenvy.com>
2017-05-19 18:03:03 +0300

    add conf for ws agent

--- dockerfiles/init/addon/modules/addon/manifests/init.pp
+++ dockerfiles/init/addon/modules/addon/manifests/init.pp
@@ -4,5 +4,15 @@ class addon {
     ensure  => "present",
     content => template("addon/openshift.properties.erb"),
     mode    => "644",
+  } ->
+  file { "/opt/che/config/plugin-conf":
+    ensure  => "directory",
+    mode    => "755",
+  } ->
+  # creating ws-agent.properties
+  file { "/opt/che/config/plugin-conf/ws-agent.properties":
+    ensure  => "present",
+    content => template("addon/ws-agent.properties.erb"),
+    mode    => "644",
   }
 }
